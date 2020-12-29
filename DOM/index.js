c7ea0f690dbf0a80a6e3c5a21644f2348f71c4f4
Vipul Chauhan <viratdev07@gmail.com>
2020-12-29 20:46:53 +0530

    getAttribute and setAttribute property of dom

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,5 @@
 // document.querySelector("h1").innerHTML = "Good Bye!";
 document.querySelectorAll("button")[0].style.backgroundColor="lightgreen";
-document.querySelector("button").classList.add("huge");
+document.querySelector("button").classList.toggle("huge");
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("a").setAttribute("href", "https://www.youtube.com");
